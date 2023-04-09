8b6af42129f0e017f03d751dfcb54f957ea6d278
Diego Noura de Brito <diego.nobri@gmail.com>
2023-04-09 01:19:52 -0300

    Endereço e Especialidade

--- Entidades/Endereco.cs
+++ Entidades/Endereco.cs
@@ -1,4 +1,5 @@
 ﻿using GisaApiArq.Dominio;
+using GisaDominio.Enum;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,13 @@ namespace GisaDominio.Entidades
 {
     public class Endereco : EntidadeBase
     {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Cidade { get; set; }
+        public UfEnum Uf { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
+
     }
 }
