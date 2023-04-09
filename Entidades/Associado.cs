95f9918ad6f39b878a859c3686b104f0ceff2eca
Diego Noura de Brito <diego.nobri@gmail.com>
2023-04-09 00:23:32 -0300

    Remoção de classes de arquitetura

--- Entidades/Associado.cs
+++ Entidades/Associado.cs
@@ -1,4 +1,5 @@
-﻿using GisaDominio.Enum;
+﻿using GisaApiArq.Dominio;
+using GisaDominio.Enum;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Entidades/Conveniado.cs
+++ Entidades/Conveniado.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GisaApiArq.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
