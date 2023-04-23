463592c2ab508f106815a18b305c264237fe2c3f
Diego Noura de Brito <diego.nobri@gmail.com>
2023-04-23 02:24:45 -0300

    AssociadoServico : aderir plano.

---
+++ Interfaces/Servicos/IServicoAssociado.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+using GisaApiArq.Servicos;
+using GisaDominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GisaDominio.Interfaces.Servicos
+{
+    public interface IServicoAssociado : IServicoCrudBase<Associado>
+    {
+        Result AderirPlano(long associadoId, long planoId);
+    }
+}
